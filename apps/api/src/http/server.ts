e382770a74f5e69a67d9c37d472d0e6c3b7b81ac
Vinicius Ponte <viniciusbponte@gmail.com>
2025-06-20 21:47:44 -0300

    feat: configuring the fastify swagger doc

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,17 +1,41 @@
-import Fastify from 'fastify'
+import fastify from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { createAccount } from './routes/auth/create-account'
+import type { ZodOpenApiVersion } from 'zod-openapi'
 import {
-  jsonSchemaTransform,
+  fastifyZodOpenApiPlugin,
+  fastifyZodOpenApiTransform,
+  fastifyZodOpenApiTransformObject,
   serializerCompiler,
   validatorCompiler,
-} from 'fastify-type-provider-zod'
-import { createAccount } from './routes/auth/create-account'
+} from 'fastify-zod-openapi'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 
-const app = Fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyZodOpenApiPlugin)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS APP with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    openapi: '3.0.3' satisfies ZodOpenApiVersion,
+  },
+  transform: fastifyZodOpenApiTransform,
+  transformObject: fastifyZodOpenApiTransformObject,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
