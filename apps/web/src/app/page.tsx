7afb3260bf6008d880a1ff7292927d7e45179a3b
Vinicius Ponte <viniciusbponte@gmail.com>
2025-07-01 00:13:19 -0300

    feat: head component

--- apps/web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from '@/auth/auth'
-
-export default async function Home() {
-  const auths = await auth()
-
-  if (auths?.user) {
-    return <pre>{JSON.stringify(auths.user, null, 2)}</pre>
-  }
-}
